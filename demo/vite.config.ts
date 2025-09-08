478c9a9db792f39f1cfd977d3028fc2126f0ae6a
Ronan <ronan.le_meillat@highcanfly.club>
2025-09-08 14:54:03 +0200

    Add @sctg/vite-plugin-github-pages-spa as a dependency and configure it in Vite

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -32,12 +32,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
 
 import packageJson from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), githubPagesSpa()],
   define: {
     __APP_VERSION__: JSON.stringify(packageJson.version),
     __VITE_VERSION__: JSON.stringify(packageJson.devDependencies.vite.replace('^', '')),
